9a38d5fb99791e2322bc7f60e9b6b9cf0874b8b6
JosephYusufov <jyusufov00@stuy.edu>
2020-01-06 10:25:03 -0500

    started making character and player

--- library/characters.h
+++ library/characters.h
@@ -1,16 +1,51 @@
-struct being;
+struct being {
+    int hp;
+    int attack;
+    int defense;
+    int level;
+    char *status[3];
+    char *ability;
+};
 
 /*---------- PROTAGONISTS ----------*/
-struct mage;
+struct mage {
+    // Default Mage config, at lv 1
+    struct being base;
+};
 
-struct knight;
+struct knight {
+    // Default Knight config, at lv 1
+    struct being base;
+};
 
-struct rogue;
+
+struct rogue {
+    // Default Rogue config, at lv 1
+    struct being base;
+};
 
 /*---------- MONSTERS ---------- */
 
-struct slime;
+struct slime {
+    // Default Slime config, at lv 1
+    struct being base;
+};
 
-struct zombie;
+struct zombie {
+    // Default zombie config, at lv 1
+    struct being base;
+};
 
-struct bat;
+struct bat {
+    // Default Rogue config, at lv 1
+    struct being base;
+};
+
+/* ---------- CREATING CHARACTERS ---------- */
+struct being * create_mage( int level );
+struct being * create_knight( int level );
+struct being * create_rogue( int level );
+struct being * create_slime( int level );
+struct being * create_zombie( int level );
+struct being * create_bat( int level );
+struct being * free_being( struct being *pointer );
