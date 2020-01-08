22f508816d3da5ed2def7857afe2b3dd21cbabdc
JosephYusufov <jyusufov00@stuy.edu>
2020-01-07 19:41:56 -0500

    created player.c file

---
+++ library/player.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+
+struct player {
+
+};
