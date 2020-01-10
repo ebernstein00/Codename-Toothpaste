1db9df288a1d5fa4213101da5f2beab1ab0ccbaf
JosephYusufov <jyusufov00@stuy.edu>
2020-01-09 21:47:00 -0500

    healthop

--- library/test.c
+++ library/test.c
@@ -14,6 +14,14 @@
 
 int main() {
     struct being *knight = create_knight(1);
+    printf("\n ----- NEW KNIGHT ----- \n");
     print_being(knight);
+
+    struct item *healthpot = create_health_potion();
+    knight->backpack[i] = healthpot;
+
+    printf("\n ----- WITH HEALTHPOT ----- \n");
+    print_being(knight);
+
     free_being(knight);
 }
