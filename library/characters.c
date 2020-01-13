4e6edb979ddc21086aeb127757da886f36118645
JosephYusufov <jyusufov00@stuy.edu>
2020-01-13 10:17:53 -0500

    hurrah

--- library/characters.c
+++ library/characters.c
@@ -18,38 +18,23 @@ struct being {
     int defense;
     int level;
     struct item **backpack; 
-    char **status;
-    char *ability;
 };
 
 /*---------- PROTAGONISTS ----------*/
-
-
 struct being * create_mage( int level ) {
     struct being *being_pointer = (struct being *)calloc(1, sizeof(struct being));
-    char **status_array = (char **)calloc(3, sizeof(char [25]));
-    status_array[0] = "";
-    status_array[1] = "";
-    status_array[2] = "";
-
     being_pointer->type = "mage";
     being_pointer->hp = 50;
     being_pointer->attack = 20;
     being_pointer->defense = 5;
     being_pointer->level = level;
     being_pointer->backpack = create_backpack();
-    being_pointer->status = status_array;
-    being_pointer->ability = "burned";
 
     return being_pointer;
 }
 
 struct being * create_knight( int level ) {
     struct being *being_pointer = (struct being *)calloc(1, sizeof(struct being));
-    char **status_array = (char **)calloc(3, sizeof(char [25]));
-    status_array[0] = "";
-    status_array[1] = "";
-    status_array[2] = "";
 
     being_pointer->type = "knight";
     being_pointer->hp = 75;
@@ -57,8 +42,6 @@ struct being * create_knight( int level ) {
     being_pointer->defense = 10;
     being_pointer->level = level;
     being_pointer->backpack = create_backpack();
-    being_pointer->status = status_array;
-    being_pointer->ability = "bleeding";
 
     return being_pointer;
 
@@ -66,10 +49,6 @@ struct being * create_knight( int level ) {
 
 struct being * create_rogue( int level ) {
     struct being *being_pointer = (struct being *)calloc(1, sizeof(struct being));
-    char **status_array = (char **)calloc(3, sizeof(char [25]));
-    status_array[0] = "";
-    status_array[1] = "";
-    status_array[2] = "";
 
     being_pointer->type = "rogue";
     being_pointer->hp = 100;
@@ -77,11 +56,8 @@ struct being * create_rogue( int level ) {
     being_pointer->defense = 15;
     being_pointer->level = level;
     being_pointer->backpack = create_backpack();
-    being_pointer->status = status_array;
-    being_pointer->ability = "poisoned";
 
     return being_pointer;
-
 }
 
 
@@ -89,30 +65,21 @@ struct being * create_rogue( int level ) {
 
 struct being * create_slime( int level ) {
     struct being *being_pointer = (struct being *)calloc(1, sizeof(struct being));
-    char **status_array = (char **)calloc(3, sizeof(char [25]));
-    status_array[0] = "";
-    status_array[1] = "";
-    status_array[2] = "";
 
     being_pointer->type = "slime";
     being_pointer->hp = 100;
     being_pointer->attack = 10;
     being_pointer->defense = 5;
     being_pointer->level = level;
-    being_pointer->backpack = create_backpack();
-    being_pointer->status = status_array;
-    being_pointer->ability = "sticky";
 
+    //BEASTS HAVE NULL BACKPACKS
+    being_pointer->backpack = NULL;
+    
     return being_pointer;
-
 }
 
 struct being * create_zombie( int level ) {
     struct being *being_pointer = (struct being *)calloc(1, sizeof(struct being));
-    char **status_array = (char **)calloc(3, sizeof(char [25]));
-    status_array[0] = "";
-    status_array[1] = "";
-    status_array[2] = "";
 
     being_pointer->type = "zombie";
     being_pointer->hp = 75;
@@ -120,8 +87,6 @@ struct being * create_zombie( int level ) {
     being_pointer->defense = 15;
     being_pointer->level = level;
     being_pointer->backpack = create_backpack();        
-    being_pointer->status = status_array;
-    being_pointer->ability = "lazy";
 
     return being_pointer;
 
@@ -129,10 +94,6 @@ struct being * create_zombie( int level ) {
 
 struct being * create_bat( int level ) {
     struct being *being_pointer = (struct being *)calloc(1, sizeof(struct being));
-    char **status_array = (char **)calloc(3, sizeof(char [25]));
-    status_array[0] = "";
-    status_array[1] = "";
-    status_array[2] = "";
 
     being_pointer->type = "bat";
     being_pointer->hp = 50;
@@ -140,15 +101,12 @@ struct being * create_bat( int level ) {
     being_pointer->defense = 5;
     being_pointer->level = level;
     being_pointer->backpack = create_backpack();        
-    being_pointer->status = status_array;
-    being_pointer->ability = "confused";
 
     return being_pointer;
 
 }
 
 struct being * free_being( struct being *pointer ) {
-    free(pointer->status);
     free_backpack(pointer->backpack);
     free(pointer);
     return pointer;
@@ -161,12 +119,6 @@ struct being * print_being( struct being *pointer) {
     printf("Defense: [%d]\n", pointer->defense);
     printf("Level: [%d]\n", pointer->level);
     print_backpack( pointer->backpack );
-    
-    printf("Status:\n");
-    printf("\t[%s]\n", pointer->status[0]);
-    printf("\t[%s]\n", pointer->status[1]);
-    printf("\t[%s]\n", pointer->status[2]);
-    printf("Ability: [%s]\n", pointer->ability);
     return pointer;
 }
 
