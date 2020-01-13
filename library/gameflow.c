d72cb99241417bf744929fb7b510dc78c59d560b
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2020-01-13 15:21:55 -0500

    Added guard_start and guard_end functions

--- library/gameflow.c
+++ library/gameflow.c
@@ -13,15 +13,26 @@
 #include "characters.h
 
 int attack(struct being * attacker, struct being * target){
-  target->hp = target->hp - (attacker->attack - target->defense);
-  return 0;
+    target->hp = target->hp - (attacker->attack - target->defense);
+    return 0;
 }
 
 int use_item(struct being * user, int item_index){
-  if (strcmp(user->backpack[item_index]->name, "Health Potion") ||
-      strcmp(user->backpack[item_index]->name, "Strength Potion")) == 0){
-    user->hp += user->backpack[item_index]->hp_buff;
-    user->attack += user->backpack[item_index]->attack_buff;
-  }
-  remove_item(user, item_index);
+    if (strcmp(user->backpack[item_index]->name, "Health Potion") ||
+        strcmp(user->backpack[item_index]->name, "Strength Potion")) == 0){
+        user->hp += user->backpack[item_index]->hp_buff;
+        user->attack += user->backpack[item_index]->attack_buff;
+    }
+    remove_item(user, item_index);
+    return 0;
+}
+
+int guard_start(struct being * user){
+    target->defense += 20;
+    return 0;
+}
+
+int guard_end(struct being * user){
+    target->defense -= 20;
+    return 0;
 }
