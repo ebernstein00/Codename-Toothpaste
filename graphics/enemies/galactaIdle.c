4ce2370f3f69c287e7b4ed95a8e3de688eadd649
jlam01 <jlam01@stuy.edu>
2020-01-15 15:36:50 -0500

    Galacta done

--- graphics/enemies/galactaIdle.c
+++ graphics/enemies/galactaIdle.c
@@ -4,7 +4,7 @@ SDL_Rect GIframe1 = {12, 344, 79, 47 }; //45 370
 SDL_Rect GIframe2 = {95, 337, 76, 52 };//128 368
 SDL_Rect GIframe3 = {178, 337, 73, 52 };// 211 369
 SDL_Rect GIframe4 = {259, 337, 73, 52 }; //292 370
-SDL_Rect GIdstrect = {0,0,200,150};
+SDL_Rect GIdstrect = {0,0,2000,150};
 int GIsprite = 0;
 SDL_Renderer* GIrenderer = NULL;
 SDL_Surface* GIimage = NULL;
@@ -22,34 +22,48 @@ void displayGalactaIdle(int x, int y) {
     GIdstrect.x = x;
     GIdstrect.y = y;
     GIsprite++;
-    GIsprite%= 24;
-    if (GIsprite < 6) {
-        SDL_RenderCopy(GIrenderer, GItexture, &GIframe1, &GIdstrect);
+    GIsprite%= 32;
+    if (GIsprite < 8) {
         GIdstrect.x = x;
         GIdstrect.y = y;
         GIdstrect.w = 316;
         GIdstrect.h = 188;
+        SDL_RenderCopy(GIrenderer, GItexture, &GIframe1, &GIdstrect);
     }
-    else if (GIsprite < 12 ) {
-        SDL_RenderCopy(GIrenderer, GItexture, &GIframe2, &GIdstrect);
+    else if (GIsprite < 16 ) {
         GIdstrect.x = x;
         GIdstrect.y = y-20;
         GIdstrect.w = 304;
         GIdstrect.h = 208;
+        SDL_RenderCopy(GIrenderer, GItexture, &GIframe2, &GIdstrect);
     }
-    else if (GIsprite < 18 ) {
-        SDL_RenderCopy(GIrenderer, GItexture, &GIframe3, &GIdstrect);
+    else if (GIsprite < 24 ) {
         GIdstrect.x = x;
         GIdstrect.y = y-24;
         GIdstrect.w = 292;
         GIdstrect.h = 208;
+        SDL_RenderCopy(GIrenderer, GItexture, &GIframe3, &GIdstrect);
     }
+    // else if (GIsprite < 32) {
+    //     GIdstrect.x = x;
+    //     GIdstrect.y = y-28;
+    //     GIdstrect.w = 292;
+    //     GIdstrect.h = 208;
+    //     SDL_RenderCopy(GIrenderer, GItexture, &GIframe4, &GIdstrect);
+    // }
+    // else if (GIsprite < 40 ) {
+    //     GIdstrect.x = x;
+    //     GIdstrect.y = y-24;
+    //     GIdstrect.w = 292;
+    //     GIdstrect.h = 208;
+    //     SDL_RenderCopy(GIrenderer, GItexture, &GIframe3, &GIdstrect);
+    // }
     else {
-        SDL_RenderCopy(GIrenderer, GItexture, &GIframe4, &GIdstrect);
         GIdstrect.x = x;
-        GIdstrect.y = y-28;
-        GIdstrect.w = 292;
+        GIdstrect.y = y-20;
+        GIdstrect.w = 304;
         GIdstrect.h = 208;
+        SDL_RenderCopy(GIrenderer, GItexture, &GIframe2, &GIdstrect);
     }
     // else if (GIsprite < GIframeTime - 5){
     //     SDL_RenderCopy(GIrenderer, GItexture, &GIframe3, &GIdstrect);
